89f76fe7a4885f12dee0aa079793be4a0327ce22
ahmedshawky2 <46525840+ahmedshawky2@users.noreply.github.com>
2019-03-09 01:12:13 +0200

    Add files via upload

---
+++ Clinic/models/customer.py
@@ -0,0 +1,16 @@
+from odoo import models, fields, api
+from odoo import exceptions
+from odoo.exceptions import ValidationError
+
+
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class customer (models.Model):
+    _inherit = 'res.partner'
+    x_child_name = fields.Char(string="Child/Sub Name", required=True, index=True, track_visibility=True,default="Parent")
+
+
+
+    _sql_constraints = [('constrainname','UNIQUE (phone,x_child_name)','Customer Phone And Child Name already exists'), ]
